7e77c1a883e0657c9c3865333235eaf8e2093ae2
samu <samuele.longo@studenti.unimi.it>
2026-07-24 12:31:32 +0200

    DOCUMENTATION: Inserita documentazione javadoc per la classe e i metodi presenti finora

--- src/main/java/annotationexporter/core/ExpCore.java
+++ src/main/java/annotationexporter/core/ExpCore.java
@@ -7,7 +7,29 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Utility class for exporting annotations from QuPath
+ */
 public class ExpCore {
+    /**
+     * Sorts the list of annotations by each annotation's hierarchy level, with annotations
+     * deeper in the hierarchy coming last.
+     * E.G:
+     *
+     * Given the list of annotations -> [C, B, E, A, D]
+     * And the following hierarchy tree ->
+     * A
+     * ├─ B
+     * │  ├─ C
+     * │  ├─ D
+     * │
+     * E
+     *
+     * The returned list will be -> [A, E, B, C, D]
+     *
+     * @param annotations the list of {@code PathObject} annotations to sort
+     * @return the sorted list of annotations
+     */
     public static @NotNull List<PathObject> sortByHierarchy(@NotNull List<PathObject> annotations) {
         List<PathObject> modifiableList = new ArrayList<>(annotations);
         modifiableList.sort(Comparator.comparingInt(PathObject::getLevel));
