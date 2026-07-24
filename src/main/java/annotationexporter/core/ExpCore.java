3e38a73db7ab66a7be3d69bdbf8722df65e45ef8
samu <samuele.longo@studenti.unimi.it>
2026-07-24 12:15:54 +0200

    VERDE: testSortByHierarchy, ordina le annotazioni rispetto alla relazione padre figlio.

--- src/main/java/annotationexporter/core/ExpCore.java
+++ src/main/java/annotationexporter/core/ExpCore.java
@@ -3,10 +3,14 @@ package annotationexporter.core;
 import org.jetbrains.annotations.NotNull;
 import qupath.lib.objects.PathObject;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ExpCore {
     public static @NotNull List<PathObject> sortByHierarchy(@NotNull List<PathObject> annotations) {
-        return List.of();
+        List<PathObject> modifiableList = new ArrayList<>(annotations);
+        modifiableList.sort(Comparator.comparingInt(PathObject::getLevel));
+        return modifiableList;
     }
 }
